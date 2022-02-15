bc815b2f2a47ba6b56b4640f1a191653f792c4f8
Carol_Inglethe <carolinainglethe@gmail.com>
2022-02-15 15:10:53 -0300

    server - getAllTasks - camada controller

---
+++ server/controllers/getAllTasksController.js
@@ -0,0 +1,10 @@
+const getAllTasksService = require('../services/getAllTasksService');
+
+module.exports = async (_req, res) => {
+  try {
+    const allTasks = await getAllTasksService();
+    return res.status(200).json(allTasks);
+  } catch (err) {
+    return res.status(500).json({ messages: 'Internal server error' });
+  }
+};
--- server/routes/tasksList.js
+++ server/routes/tasksList.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'Sucesso' });
-});
+const getAllTasksController = require('../controllers/getAllTasksController');
+
+router.get('/', getAllTasksController);
 
 module.exports = router;
