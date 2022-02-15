f1dde684981923d9080f0993da9e95b69acf9b66
Carol_Inglethe <carolinainglethe@gmail.com>
2022-02-15 17:58:21 -0300

    server - inicia rota createTask

--- server/routes/tasksList.js
+++ server/routes/tasksList.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const getAllTasksController = require('../controllers/getAllTasksController');
+const createTaskController = require('../controllers/createTaskController');
 
 router.get('/', getAllTasksController);
+router.post('/', createTaskController);
 
 module.exports = router;
