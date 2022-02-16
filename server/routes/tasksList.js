6536aa8cdcb6a6c9cc28c3f0dde00961eda8030d
Carol_Inglethe <carolinainglethe@gmail.com>
2022-02-15 21:00:43 -0300

    server - updateTask - iniciando rota

--- server/routes/tasksList.js
+++ server/routes/tasksList.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const getAllTasksController = require('../controllers/getAllTasksController');
 const createTaskController = require('../controllers/createTaskController');
+const updateTaskController = require('../controllers/updateTaskController');
 
 router.get('/', getAllTasksController);
 router.post('/', createTaskController);
+router.put('/:name', updateTaskController);
 
 module.exports = router;
